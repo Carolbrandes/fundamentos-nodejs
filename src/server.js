b9187d2f77ac86777109cf330fdeadb1e946fd2d
carol <carolbrandes23@gmail.com>
2024-09-05 19:27:45 -0400

    route with params

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import http from "node:http"
 import { json } from "./middlewares/json.js"
 import { routes } from "./routes.js"
 
+// Query Parameters: URL Stateful -> Filtros, paginação, não obrigatórios
+// ex: http://localhost:3333/users?userId=1
+
+// Route Parameters: Identificação de recursos
+// ex: http://localhost:3333/users/1
+
+// Request Body: Envio de informacoes de um formulario (HTTPS)
+
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -10,9 +18,11 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
 
-    const route = routes.find(route => route.method === method && route.path === url)
+    const route = routes.find(route => route.method === method && route.path.test(url))
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+        console.log("🚀 ~ server ~ routeParams:", routeParams)
         return route.handler(req, res)
     }
 
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,9 @@
+export function buildRoutePath(path) {
+    const routeParametersRegex = /:([a-zA-Z]+)/g // aqui busca o nome do campo q esta sendo enviado
+
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)') // esse $1 pega o que primeiro retorno de routeParametersRegex e nomeia o grupo da regex.
+
+    const pathRegex = new RegExp(`ˆ${pathWithParams}`)
+
+    return pathRegex
+}
